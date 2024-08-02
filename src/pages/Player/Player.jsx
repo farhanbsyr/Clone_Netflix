bafa5b76a28f859d2ff3a15592ec03158d7ad766
unknown <farhanalktr91@gmail.com>
2024-08-02 23:44:13 +0700

    menambahkan pages player

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 import "./Player.css";
+import back_arrow from "../../assets/back_arrow_icon.png";
 const Player = () => {
-  return <div>Player</div>;
+  return (
+    <div className="player">
+      <img src={back_arrow} alt="back arrow" />
+      <iframe
+        src={`https://www.youtube.com/embed/YQQD67N5pi0&t=6738s`}
+        width="90%"
+        title="trailer"
+        frameborder="0"
+        allowFullScreen
+        height="90%"
+        frameBorder="0"
+      ></iframe>
+      <div className="player-info">
+        <p>Published Date</p>
+        <p>Name</p>
+        <p>Type</p>
+      </div>
+    </div>
+  );
 };
 
 export default Player;
