6af0b38790bbeb5aed1c70c776a94da146f57b2b
unknown <farhanalktr91@gmail.com>
2024-08-03 07:59:00 +0700

    test path

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import Home from "./pages/Home/Home";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Router } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Player from "./pages/Player/Player";
 
 const App = () => {
   return (
-    <div>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/player/:id" element={<Player />} />
       </Routes>
-    </div>
+    </Router>
   );
 };
 
